24b2f08f6699b77fb50c3476c36cfc16a0250bdc
Nikhesh <nikhesh.p@kambala.co.in>
2024-01-10 16:05:20 +0530

    get_limits added

--- src/main/java/com/noren/javaapi/NorenRoutes.java
+++ src/main/java/com/noren/javaapi/NorenRoutes.java
@@ -26,6 +26,7 @@ public class NorenRoutes {
            put("cancelorder", "/CancelOrder");
            put("timepriceseries", "/TPSeries");
            put("forgotpassword_OTP", "/FgtPwdOTP"); 
+           put("get_limits", "/Limits"); 
         }};
        }
        public String get(String key){
