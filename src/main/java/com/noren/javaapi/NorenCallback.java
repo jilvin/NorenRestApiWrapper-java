696cc2bf6879dd7b3b90a24e9a59c6839ced3c7c
Nikhesh <nikhesh.p@kambala.co.in>
2024-06-26 11:22:49 +0530

    initial commit

---
+++ src/main/java/com/noren/javaapi/NorenCallback.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.noren.javaapi;
+
+/**
+ *
+ * @author ubuntu
+ */
+public interface NorenCallback
+{
+    public void Onfeed(String msg);
+    
+}
